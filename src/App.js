16772493d5c3fec705dc3475e3c66cf69af487f8
MurilloCunha <jcunhamurillo@gmail.com>
2021-08-11 16:13:50 -0300

    add App structure

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { BrowserRouter,Route, Switch } from 'react-router-dom';
 
 import './App.css';
 import Header from './components/header';
+import Navigation from './components/navigation';
 
 function App() {
+  const [user,setUser] = useState({logged:true})
   return (
     <div className="App">
-      <Header />
+      <BrowserRouter>
+        <Header />
+        <main>
+          <Switch>
+            <Route path="/home"></Route>
+            <Route path="/login"></Route>
+            <Route path="/receitas"></Route>
+            <Route path="/nova-receita"></Route>
+          </Switch>
+        </main>
+        {user.logged &&
+          <footer>
+              <Navigation />
+          </footer>
+        }
+      </BrowserRouter>
     </div>
   );
 }
