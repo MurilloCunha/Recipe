7ea92aa481390b7ea20e845a1972cbc41f31f9af
MurilloCunha <jcunhamurillo@gmail.com>
2021-08-16 17:10:38 -0300

    update text input (add label)

--- src/components/TextInput/index.jsx
+++ src/components/TextInput/index.jsx
@@ -1,13 +1,28 @@
-import React from 'react'
+import React, {useCallback, useState} from 'react'
 
 import PropTypes from 'prop-types'
 
 function TextInput(props) {
+  const [empty,setEmpty] = useState("empty")
+
+  const handleChange= useCallback((event) => {
+    const { value } = event.target
+    setEmpty(value.length > 0 ? value : "empty")
+  },[])
+
   return (
-    <input 
-    className={`text-input`}
-    data-error={props.error}
-    {...props} error="" />
+    <div className="text-input-wrapper">
+      <input 
+        className={`text-input`}
+        data-error={props.error}
+        data-variant={props.variant}
+        {...props}
+        data-empty={empty}
+        onChange={handleChange}
+        id={props.name}
+      />
+      {props.label && <label htmlFor={props.name} >{props.label}</label>}
+    </div>
   )
 }
 
@@ -15,4 +30,14 @@ export default TextInput
 
 TextInput.propTypes={
   error: PropTypes.bool,
+  variant: PropTypes.string,
+  label: PropTypes.string,
+  name: PropTypes.string
+}
+
+TextInput.defaultProps = {
+  error:"false",
+  variant:"",
+  label:undefined,
+  name:"input"
 }
