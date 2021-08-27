a8e6d320f4b217f0daf8f71f12bf1a90906bb066
MurilloCunha <jcunhamurillo@gmail.com>
2021-08-27 16:36:29 -0300

    add Provider to app

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { BrowserRouter,Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import './App.css';
 import Header from './components/header';
@@ -8,35 +8,39 @@ import Navigation from './components/navigation';
 import LoginPage from './pages/login';
 import NewRecipe from './pages/nova-receita';
 import Recipes from './pages/receitas';
+import Provider from './components/provider';
 
 function App() {
-  const [user,setUser] = useState({logged:false})
+  const [user, setUser] = useState({ logged: false })
 
   return (
     <div className="App">
-      <BrowserRouter>
-        <Header user={user} setUser={setUser}/>
-        <main>
-          <Switch>
-          <Route path="/home">
-          </Route>
-          <Route path="/receitas">
-            <Recipes />
-          </Route>
-          <Route path="/login">
-            <LoginPage user={user} setUser={setUser}/>
-          </Route>
-          <Route path="/nova-receita">
-            <NewRecipe />
-          </Route>
-          </Switch>
-        </main>
-        {user.logged &&
-          <footer>
-              <Navigation setUser={setUser}/>
-          </footer>
-        }
-      </BrowserRouter>
+      <Provider>
+        <BrowserRouter>
+          <Header user={user} setUser={setUser} />
+          <main>
+            <Switch>
+              <Route path="/home">
+              </Route>
+              <Route path="/receitas">
+                <Recipes />
+              </Route>
+              <Route path="/login">
+                <LoginPage user={user} setUser={setUser} />
+              </Route>
+              <Route path="/nova-receita">
+                <NewRecipe />
+              </Route>
+            </Switch>
+          </main>
+          {user.logged &&
+            <footer>
+              <Navigation setUser={setUser} />
+            </footer>
+          }
+        </BrowserRouter>
+      </Provider>
+
     </div>
   );
 }
