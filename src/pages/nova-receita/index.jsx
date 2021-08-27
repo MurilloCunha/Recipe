cec6611ed60bd4bd59140ac6a6d054b373815951
MurilloCunha <jcunhamurillo@gmail.com>
2021-08-27 15:57:32 -0300

    nova-receita page update

--- src/pages/nova-receita/index.jsx
+++ src/pages/nova-receita/index.jsx
@@ -1,68 +1,112 @@
-import React, { useCallback } from 'react'
+import React, { Fragment, useCallback, useContext, useState } from 'react'
 
 import ImageInput from '../../components/image-input'
 import IngredientsList from '../../components/ingredients-list'
+import RecipeStepList from '../../components/recipe-step-list'
 import TextInput from '../../components/TextInput'
+import Button from '../../components/button'
+import RecipeBookContext from '../../context/configure-context'
+import PotLoader from '../../components/pot-loader'
+import { useHistory } from 'react-router-dom'
+import SelectInput from '../../components/select-input'
+import { CATEGORY_OPTIONS, PUBLISHING_OPTIONS, SERVING_OPTIONS, TIME_OPTIONS } from '../../utils/constants'
+import { publishSimulator } from '../../utils/test'
+
 
 function NewRecipe() {
+  const { newRecipe, recipeActions } = useContext(RecipeBookContext)
+  const [state, setState] = useState("editing")
+  const [error, setError] = useState([])
+  const history = useHistory()
 
-  const handleSubmit = useCallback((event)=> {
+  const handleErrors = () => {
+    let activeErrors = []
+    if (newRecipe.steps.length < 1) {
+      activeErrors.push("step_error")
+    }
+
+    if (newRecipe.ingredients.length < 1) {
+      activeErrors.push("ingredient_error")
+    }
+    setError(activeErrors)
+
+    return activeErrors.length > 0
+  }
+
+  const handleSubmit = useCallback((event) => {
     event.preventDefault()
-
     const formData = new FormData(event.target)
+    const ignore = ["ingredient", "image", "description", "time"]
+    formData.append("fotoUrl", URL.createObjectURL(formData.get("foto")))
 
-    console.log(formData.get('recipeFoto'))
-  },[])
+    for (var pair of formData.entries()) {
+      if (!ignore.some(key => key == pair[0])) {
+        recipeActions.addProperties({ [pair[0]]: pair[1] })
+      }
+    }
 
+    const hasError = handleErrors()
+    if (!hasError) {
+      setState("publishing")
+      publishSimulator(newRecipe)
+        .then((result) => {
+          setState(result)
+            setTimeout(() => {
+              recipeActions.reset()
+              history.push("/receitas")
+            }, 3000)
+        })
+        .catch((result) => {
+          setState(result)
+          setTimeout(() => {
+            setState("editing")
+          }, 3000)
+        })
+    }
+  }, [newRecipe])
+
+  const handleCancel = useCallback(() => {
+    recipeActions.reset()
+    history.push('/receitas')
+  }, [])
   return (
     <section className="new-recipe-wrapper">
       <h1>Nova Receita</h1>
-      <form onSubmit={handleSubmit}>
-        <article className="new-recipe__description">
-          <ImageInput name="recipeFoto"/>
-          <div className="new-recipe__info">
-            <TextInput type="text" variant="form" label="Nome" name="recipeName" required/>
-            <select name="category" required>
-              <option value="">Categoria</option>
-              <option value="Bolos">Bolos</option>
-              <option value="Tortas">Tortas</option>
-              <option value="Doces">Doces</option>
-              <option value="Massa">Massas</option>
-              <option value="Carnes">Carnes</option>
-              <option value="Aves">Aves</option>
-              <option value="Peixes">Peixes</option>
-              <option value="Frutos-do-mar">Frutos do mar</option>
-              <option value="Saladas">Saladas</option>
-              <option value="Sopas">Sopas</option>
-              <option value="Bebidas">Bebidas</option>
-              <option value="Lanches">Lanches</option>
-            </select>
-            <div className="new-recipe__properties" required>
-              <select name="preparationTime">
-                <option value="">Tempo</option>
-                <option value="10-30">10-30min.</option>
-                <option value="30-60">30-60min.</option>
-                <option value="60-120">1h-2h</option>
-                <option value=">120">mais de 2h</option>
-              </select>
-              <select name="serve" required>
-                <option value="">serve</option>
-                <option value="1">1 pessoa</option>
-                <option value="1-2">1-2 pessoas</option>
-                <option value="2-4">2-4 pessoas</option>
-              </select>
+      {state === "editing" &&
+        <form onSubmit={handleSubmit}>
+          <article className="new-recipe__description">
+            <ImageInput id="recipeFoto" name="foto" defaultValue={newRecipe.fotoUrl} />
+            <div className="new-recipe__info">
+              <TextInput type="text" variant="form" label="Nome" name="name" defaultValue={newRecipe.name} required />
+              <SelectInput name="category" defaultValue={newRecipe.category} options={CATEGORY_OPTIONS} required />
+              <div className="new-recipe__properties" required>
+                <SelectInput name="recipeTime" defaultValue={newRecipe.recipeTime} options={TIME_OPTIONS} required />
+                <SelectInput name="serving" defaultValue={newRecipe.serving} options={SERVING_OPTIONS} required />
+              </div>
             </div>
+          </article>
+          <article className="new-recipe__ingredients">
+            <h2>Ingredientes</h2>
+            {error.includes("ingredient_error") && <p className="new-recipe__error">adicione pelo menos um ingrediente.</p>}
+            <IngredientsList />
+          </article>
+          <article className="new-recipe__steps">
+            <h2>Passos</h2>
+            {error.includes("step_error") && <p className="new-recipe__error">adicione pelo menos um passo.</p>}
+            <RecipeStepList />
+          </article>
+          <div className="new-recipe__controls">
+            <Button variant="primary" onClick={handleCancel}>Cancelar</Button>
+            <Button variant="secondary">Publicar</Button>
           </div>
-        </article>
-        <article className="new-recipe__ingredients">
-          <h2>Ingredientes</h2>
-          <IngredientsList />
-        </article>
-        <article className="new-recipe__steps">
-          <h2>Passos</h2>
-        </article>
-          <button type="submit">TESTE</button>
-      </form>
+        </form>
+      }
+      {state !== "editing" &&
+        <Fragment>
+          <PotLoader state={state} />
+          <p className="new-recipe__message">{PUBLISHING_OPTIONS[state]}</p>
+        </Fragment>
+      }
     </section>
   )
 }
