cf5839a0a007f24cd239fb9887639158ed44a1d9
MurilloCunha <jcunhamurillo@gmail.com>
2021-08-27 15:53:05 -0300

    image-input update

--- src/components/image-input/index.jsx
+++ src/components/image-input/index.jsx
@@ -4,20 +4,20 @@ import PropTypes from 'prop-types'
 import imgPlaceholder from '../../images/tray.svg'
 
 
-function ImageInput({name}) {
+function ImageInput({id,name, required,getFile, defaultValue}) {
   const [uploadedImage, setUploadedImage]= useState(imgPlaceholder)
-
 
   const handleChange = useCallback((event) => {
     const uploadedImageURL = URL.createObjectURL(event.target.files[0])
+    getFile && getFile({target:{value:event.target.files[0], name}})
     setUploadedImage(uploadedImageURL)
   })
 
   return (
     <div className="image-input">
-      <input type="file" id={name} name={name} onChange={handleChange} accept="image/*" capture="camera"/>
-      <label htmlFor={name} >
-        <img src={uploadedImage} alt="adiciona uma foto da sua receita" />
+      <input type="file" id={id} name={name} onChange={handleChange} accept="image/*" capture="camera" required={required}/>
+      <label htmlFor={id} >
+        <img src={defaultValue || uploadedImage} alt="adiciona uma foto da sua receita" />
       </label>
     </div>
   )
@@ -27,8 +27,15 @@ export default ImageInput
 
 ImageInput.propTypes = {
   name: PropTypes.string,
+  required: PropTypes.bool,
+  id: PropTypes.string,
+  defaultValue: PropTypes.string,
+  getFile: PropTypes.func,
 }
 
 ImageInput.defaultProps ={
-  name:"image"
+  name:"image",
+  required:false,
+  id:"",
+  defaultValue:undefined
 }
